6bb432d68cf48c23126a135654e8e150aefa80e5
gfrank975 <franktep975@gmail.com>
2025-03-31 10:11:44 +0700

    Store data

--- store_data_base.py
+++ store_data_base.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel
 from connect import get_weather
-
+from terrain_api import get_terrain
 
 
 app = FastAPI()
@@ -11,8 +11,8 @@ app = FastAPI()
 mongo_url = "mongodb://localhost:27017"
 client = AsyncIOMotorClient(mongo_url)
 db = client.mydatabase
-collection = db.weather_forecast
-
+collection1 = db.weather_forecast
+collection2 = db.terrain
 
 class Data(BaseModel):
     date: str
@@ -24,10 +24,22 @@ class Data(BaseModel):
     slp: float
     condition: str
 
-#add data to mongo db
+class Data2(BaseModel):
+    latitude: float 
+    longitude: float
+    ชั้นความสูงM : float
+    สิ่งปลูกสร้าง : str
+    ภูมิประเทศ : str
+    พืชพรรณป่าไม้ : str
+
+
+
+
+#add data weather to mongo db
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////
 @app.post("/weather/fetch")
 async def create_item(province:str,amphoe:str,tambon:str):
-    get_api = get_weather(province,amphoe,tambon)
+    get_api = await get_weather(province,amphoe,tambon)
     data_dict = {
         "date": get_api["date"],
         "latitude": get_api["latitude"], 
@@ -38,16 +50,48 @@ async def create_item(province:str,amphoe:str,tambon:str):
         "slp": get_api["slp (hpa)"],
         "condition": get_api["condition"],
     }
-    result = await collection.insert_one(data_dict)
+    result = await collection1.insert_one(data_dict)
     return {"id": str(result.inserted_id),
             "province":province,
             "Amphoe":amphoe,
             "Tambon":tambon,
             "message":"weather data added"}
+#////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+#////////////////////////////////////////////////////////////////////////////////////////////////////
+#add data terrain to mongo db
+@app.post("/terrain/fetch")
+async def create_item_terrain(lat:float,lon:float,radius:int):
+    get_api = await get_terrain(lat,lon,radius)
+    data_dict = {
+        "latitude": get_api["Latitude"], 
+        "longitude": get_api["Lontitude"],
+        "ชั้นความสูงM" : get_api["ชั้นความสูง(M)"],
+        "สิ่งปลูกสร้าง" : get_api["สิ่งปลูกสร้าง"] ,
+        "ภูมิประเทศ" : get_api["ภูมิประเทศ"],
+        "พืชพรรณป่าไม้": get_api["พืชพรรณป่าไม้"] 
+    }
+    result = await collection2.insert_one(data_dict)
+    return {"id": str(result.inserted_id),
+            "lat":lat,
+            "lon": lon,
+            "message":"weather data added"}
+#////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+#get data
 @app.get("/weather/")
 async def get_item():
-    datas = await collection.find().to_list(100)
+    datas = await collection1.find().to_list(100)
     for data in datas:
         data["_id"] = str(data["_id"])
     return datas
+
+@app.get("/terrain/")
+async def get_item():
+    datas_2 = await collection2.find().to_list(100)
+    for data in datas_2:
+        data["_id"] = str(data["_id"])
+    return datas_2
